16a3427c24643ddeac49524eba11b55be725acec
Osmany Montero <osmontero@icloud.com>
2022-01-16 23:49:39 +0300

    wazuh agent on linux

--- uninstall.go
+++ uninstall.go
@@ -1,6 +1,9 @@
 package main
 
-import "runtime"
+import (
+	"path/filepath"
+	"runtime"
+)
 
 func uninstall() error {
 	path, err := getMyPath()
@@ -9,11 +12,32 @@ func uninstall() error {
 	}
 	switch runtime.GOOS {
 	case "windows":
-		_, err := execute("nssm.exe", path, "stop", "utmagent")
+		_, err := execute("nssm.exe", path, "stop", "utmstack")
 		if err != nil {
 			return err
 		}
-		_, err = execute("nssm.exe", path, "remove", "utmagent")
+
+		_, err = execute("nssm.exe", path, "remove", "utmstack")
+		if err != nil {
+			return err
+		}
+	case "linux":
+		_, err = execute("systemctl", path, "disable", "utmstack")
+		if err != nil {
+			return err
+		}
+
+		_, err = execute("systemctl", path, "stop", "utmstack")
+		if err != nil {
+			return err
+		}
+
+		_, err = execute("rm", path, filepath.Join("/", "etc", "systemd", "system", "utmstack.service"))
+		if err != nil {
+			return err
+		}
+
+		_, err = execute("systemctl", path, "daemon-reload")
 		if err != nil {
 			return err
 		}
--- wazuh.go
+++ wazuh.go
@@ -37,7 +37,7 @@ func configureWazuh(ip, key string) error {
 	}
 
 	type WazuhConfig struct {
-		IP  string
+		IP string
 	}
 
 	dKey, err := base64.StdEncoding.DecodeString(key)
@@ -60,6 +60,103 @@ func configureWazuh(ip, key string) error {
 		err = writeToFile(filepath.Join(path, "wazuh", "windows", "client.keys"), string(dKey[:]))
 		if err != nil {
 			return err
+		}
+	case "linux":
+		var templateFile string
+		configFile := filepath.Join("/", "var", "ossec", "etc", "ossec.conf")
+
+		family, err := detectLinuxFamily()
+		if err != nil {
+			return err
+		}
+
+		switch family {
+		case "debian":
+			templateFile = filepath.Join(path, "templates", "wazuh-debian.template")
+
+			_, err := execute("apt", filepath.Join(path, "wazuh"), "update")
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("apt", filepath.Join(path, "wazuh"), "install", "-y", "curl", "apt-transport-https", "lsb-release", "gnupg2", "wget")
+			if err != nil {
+				return err
+			}
+
+			err = download("https://packages.wazuh.com/key/GPG-KEY-WAZUH")
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("apt-key", path, "add", "GPG-KEY-WAZUH")
+			if err != nil {
+				return err
+			}
+
+			err = writeToFile(filepath.Join("/", "etc", "apt", "sources.list.d", "wazuh.list"), "deb https://packages.wazuh.com/4.x/apt/ stable main")
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("apt", filepath.Join(path, "wazuh"), "update")
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("apt", filepath.Join(path, "wazuh"), "install", "-y", "wazuh-agent")
+			if err != nil {
+				return err
+			}
+
+		case "rhel":
+			templateFile = filepath.Join(path, "templates", "wazuh-rhel.template")
+
+			_, err := execute("rpm", filepath.Join(path, "wazuh"), "--import", "https://packages.wazuh.com/key/GPG-KEY-WAZUH")
+			if err != nil {
+				return err
+			}
+
+			err = writeToFile(
+				filepath.Join("/", "etc", "yum.repos.d", "wazuh.repo"),
+				`[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/4.x/yum/
+protect=1`,
+			)
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("yum", filepath.Join(path, "wazuh"), "install", "-y", "wazuh-agent")
+			if err != nil {
+				return err
+			}
+		}
+
+		if family == "debian" || family == "rhel" {
+			err = generateFromTemplate(config, templateFile, configFile)
+			if err != nil {
+				return err
+			}
+
+			err = writeToFile(filepath.Join("/", "var", "ossec", "etc", "client.keys"), string(dKey[:]))
+			if err != nil {
+				return err
+			}
+
+			_, err := execute("systemctl", filepath.Join(path, "beats"), "enable", "wazuh-agent")
+			if err != nil {
+				return err
+			}
+
+			_, err = execute("systemctl", filepath.Join(path, "beats"), "restart", "wazuh-agent")
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
