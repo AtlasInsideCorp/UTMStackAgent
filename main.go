f4c9d745feeb89a6184a7a133e4380d2bcf8949d
Osmany Montero <osmontero@icloud.com>
2022-01-20 01:27:24 +0300

    uninstall if the service is already installed

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -38,13 +37,6 @@ func main() {
 	if len(os.Args) > 1 {
 		arg := os.Args[1]
 		switch arg {
-		case "uninstall":
-			err := uninstall()
-			if err != nil {
-				h.FatalError("can't remove agent dependencies or configurations: %v", err)
-			}
-			
-			os.Exit(0)
 		case "run":
 			incidentResponse()
 			startBeat()
@@ -52,55 +44,62 @@ func main() {
 			signals := make(chan os.Signal, 1)
 			signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 			<-signals
+			stopWazuh()
 		default:
 			fmt.Println("unknown option")
 		}
 	} else {
-		var ip string
-		var utmKey string
+		if _, err := os.ReadFile(); err != nil {
 
-		fmt.Println("Manager IP or FQDN:")
-		if _, err := fmt.Scanln(&ip); err != nil {
-			h.FatalError("can't get the manager IP or FQDN: %v", err)
+			var ip string
+			var utmKey string
+
+			fmt.Println("Manager IP or FQDN:")
+			if _, err := fmt.Scanln(&ip); err != nil {
+				h.FatalError("can't get the manager IP or FQDN: %v", err)
+			}
+
+			fmt.Println("Registration Key:")
+			if _, err := fmt.Scanln(&utmKey); err != nil {
+				h.FatalError("can't get the registration key: %v", err)
+			}
+
+			hostName, err := os.Hostname()
+			if err != nil {
+				h.FatalError("can't get the hostname: %v", err)
+			}
+
+			agent, err := registerAgent(AGENTMANAGERPROTO+"://"+ip+":"+strconv.Itoa(AGENTMANAGERPORT), hostName, utmKey)
+			if err != nil {
+				h.FatalError("Can't register agent: %v", err)
+			}
+
+			cnf := config{Server: ip, AgentID: agent.ID, AgentKey: agent.Key}
+			err = writeConfig(cnf)
+			if err != nil {
+				h.FatalError("can't write agent config: %v", err)
+			}
+
+			err = configureBeat(ip)
+			if err != nil {
+				h.FatalError("can't configure beat: %v", err)
+			}
+
+			err = configureWazuh(ip, cnf.AgentKey)
+			if err != nil {
+				h.FatalError("can't configure wazuh: %v", err)
+			}
+
+			err = autoStart()
+			if err != nil {
+				h.FatalError("can't configure agent service: %v", err)
+			}
+		} else {
+			err := uninstall()
+			if err != nil {
+				h.FatalError("can't remove agent dependencies or configurations: %v", err)
+			}
 		}
-
-		fmt.Println("Registration Key:")
-		if _, err := fmt.Scanln(&utmKey); err != nil {
-			h.FatalError("can't get the registration key: %v", err)
-		}
-
-		hostName, err := os.Hostname()
-		if err != nil {
-			h.FatalError("can't get the hostname: %v", err)
-		}
-
-		agent, err := registerAgent(AGENTMANAGERPROTO+"://"+ip+":"+strconv.Itoa(AGENTMANAGERPORT), hostName, utmKey,
-		)
-		if err != nil {
-			h.FatalError("Can't register agent: %v", err)
-		}
-
-		cnf := config{Server: ip, AgentID: agent.ID, AgentKey: agent.Key}
-		err = writeConfig(cnf)
-		if err != nil {
-			h.FatalError("can't write agent config: %v", err)
-		}
-
-		err = configureBeat(ip)
-		if err != nil {
-			h.FatalError("can't configure beat: %v", err)
-		}
-
-		err = configureWazuh(ip, cnf.AgentKey)
-		if err != nil {
-			h.FatalError("can't configure wazuh: %v", err)
-		}
-
-		err = autoStart()
-		if err != nil {
-			h.FatalError("can't configure agent service: %v", err)
-		}
-
 		os.Exit(0)
 	}
 }
