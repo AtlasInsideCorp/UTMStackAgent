aade6cebb7b1ae9cf1a8fb7e6e4a0c88cdae32d0
Osmany Montero <osmontero@icloud.com>
2022-01-22 14:57:02 +0300

    enhance distribution detection function

--- os.go
+++ os.go
@@ -3,16 +3,25 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func detectLinuxFamily() (string, error) {
 	var pmCommands map[string]string = map[string]string{
-		"debian": "dpkg-architecture",
+		"debian": "apt list",
 		"rhel":   "yum",
 	}
 
 	for dist, command := range pmCommands {
-		_, err := exec.Command(command).Output()
+		cmd := strings.Split(command, " ")
+		var err error
+		
+		if len(cmd) > 1 {
+			_, err = exec.Command(cmd[0], cmd[1:]...).Output()
+		} else {
+			_, err = exec.Command(cmd[0]).Output()
+		}
+		
 		if err == nil {
 			return dist, nil
 		}
