7362737360dd8aebd0361acdd151581e545be50d
Yorjander Hernández Vergara <yorjander@atlasinside.com>
2023-05-12 11:59:50 -0400

    Get ip address

---
+++ utils/address.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"errors"
+	"net"
+)
+
+// GetIPAddress gets the IP address of the system's active network interface.
+func GetIPAddress() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		ipNet, ok := addr.(*net.IPNet)
+		if ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("failed to get IP address")
+}
